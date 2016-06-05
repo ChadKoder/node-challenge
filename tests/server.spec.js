064f32c5cd1988003a61947da8ea340a1875817b
Chad Keibler <chad.keibler@aesynt.com>
2016-06-05 09:55:57 -0400

    added unit test to make sure tests run

---
+++ tests/server.spec.js
@@ -0,0 +1,8 @@
+//var request = require('request');
+describe('server', function() {
+	describe('test1', function() {
+		it('should do something', function() {
+			expect(1).toEqual(1);
+		});
+	});
+});
