c986202e552d9228a92f1bb962c5e15918db157f
Chad Keibler <chad.keibler@aesynt.com>
2016-06-05 15:10:27 -0400

    adding empty functions as a reminder to build this functionality out... Will do this shortly.

--- MainCtrl.js
+++ MainCtrl.js
@@ -1,10 +1,16 @@
 angular.module('sampleApp.controllers', []).
 controller('MainCtrl', function($scope) {
+	$scope.totalStairs = 0;
+	
 	$scope.logout = function (){
 		alert('logging out');
 	}
 	
 	$scope.isValid = function(){
+		
+	}
+	
+	$scope.showStairs = function(total) {
 		
 	}
 });
