e58e582600bc48429195a3a9dde12eb68df98345
Chad Keibler <chad.keibler@aesynt.com>
2016-06-05 15:16:57 -0400

    added comment to build.sh script. Added first test for api - attemptLogin without creditials returns 401 unauthorized

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,8 +1,19 @@
-//var request = require('request');
+var supertest = require('supertest');
+var server = require('../server');
+var api = supertest('http://localhost:8888');
+
 describe('server', function() {
 	describe('test1', function() {
 		it('should do something', function() {
 			expect(1).toEqual(1);
+		});
+	});
+	
+	describe('GET /attemptLogin with no credentials', function(){
+		it('should return status code 401 Unauthorized', function(done){
+			 api.get('/attemptLogin')
+				.set('Accept', 'application/json')
+				.expect(401, done);
 		});
 	});
 });
