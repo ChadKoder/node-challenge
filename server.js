24b8c5a6b6c0e8da08625872d6abc2a35fe8589d
Chad Keibler <chad.keibler@aesynt.com>
2016-06-06 14:06:17 -0400

    moved files to src folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http'),
 	url = require('url'),
 	path = require('path'),
 	fs = require('fs'),
-	configs = require('./configurations.json'),
+	configs = require('./src/configurations.json'),
 	token = '',
 	childProcess = require('child_process'),
 	browserToLaunch = '',
@@ -15,7 +15,7 @@ const PORT = 8888;
 if (arg){
 	userArg = arg.toLowerCase();
 }
-
+	
 if (userArg){
 	console.log('attempting to run node with ' + userArg + '...');
 	
@@ -24,7 +24,7 @@ if (userArg){
 			fs.statSync(chrome);
 			browserToLaunch = chrome;
 		} catch(e) {
-			//console.log('chrome does not exist, setting to launch with iexplore');
+			console.log('chrome does not exist, setting to launch with iexplore');
 			browserToLaunch = iexplore;
 		}
 	} else if (userArg === 'iexplore'){
@@ -32,14 +32,12 @@ if (userArg){
 			fs.statSync(iexplore);
 			browserToLaunch = iexplore;
 		} catch(e) {
-			//console.log('iexplore does not exist, please manually launch your browser and navigate to "http://localhost:8888"');
+			console.log('iexplore does not exist, please manually launch your browser and navigate to "http://localhost:8888"');
 			browserToLaunch = '';
 		}
 	} else if (userArg === 'tests/') {
 		browserToLaunch = null;
 		return;
-	} else {
-		//console.log('defined parameter does not exist! Continuing...');
 	}
 }
 
@@ -83,14 +81,12 @@ var setContentType = function (url) {
 var write200SuccessResponse = function (res, file, contentType){
 	if (token) {
 		if (file){
-			//console.log('writing token and file');
 			res.writeHeader(200, {'Content-Type': contentType},
 		{'Authorization': 'Basic ' + token});
 			res.write(file, 'binary');
 			res.end();
 			return;
 		} else {
-			//console.log('writing token with no file');
 			res.writeHead(200);
 			res.write('200 OK');
 			res.end();
@@ -101,21 +97,17 @@ var write200SuccessResponse = function (res, file, contentType){
 		return;
 	} else {
 		if (file){
-			//console.log('writing NO token with a file');
 			res.writeHead(200);
 			res.write(file, 'binary');
 			res.end();
-			//console.log('made it to No token with file END');
 			return;
 		} else {
-			//console.log('writing NO token and NO file');
 			res.writeHead(200);
 			res.write('200 OK');
 			res.end();
 			return;
 		}
 	}
-	
 };
 
 var write404NotFoundResponse = function(res, contentType){
@@ -136,68 +128,75 @@ var write401Unauthorized = function(res, contentType){
 	res.end();
 };
 
+var renderFile = function (res, fileName, contentType){
+	fs.readFile(fileName, 'binary', function(err, file){
+		if (err) {
+			console.log('error for file : ' + fileName + ' err: ' + err);
+			write500InternalErrorResponse(res, err, contentType);
+			return;
+		}
+		
+		write200SuccessResponse(res, file, contentType);
+	});
+};
+
 http.createServer(function (req, res) {
 	var contentType, currentWorkingDir = process.cwd(),
 	uri = url.parse(req.url).pathname,
 	fileName = path.join(currentWorkingDir, uri),
 	contentType = setContentType(req.url);
 	
-	if (uri.indexOf('/configs') > -1){
-		res.setHeader('Content-Type', 'application/json');
-		res.write(JSON.stringify(configs));
-		res.end();
-		return;
-	} 
-
-	if (uri.indexOf('/attemptLogin') > -1){
-		//console.log('attempting login...');
-		var credentials = url.parse(req.url, true).query;
-		
-		if (credentials){
-			var username = credentials.username;
-			var password = credentials.password;
-			
-			if (!validate(username, password)){
-				write401Unauthorized(res);
-				return;
-			} else {
-				token = new Buffer('username:' + username + ',' + 'password:' + password).toString('base64');
-				write200SuccessResponse(res, null, contentType);
-				return;
-			}
-		}
-		
-		//console.log('failed on /attemptLogin');
-		//write404NotFoundResponse(res, contentType);
+	if ((uri.indexOf('node_modules') > -1) || uri.indexOf('src') > -1){
+		renderFile(res, fileName, contentType);
 		return;
 	}
-		
-	if (fs.statSync(fileName).isDirectory()) {
-		fileName += 'index.html';
-		//console.log('reading index.html');
-	}// else {
-		//if (url.indexOf('configs') > -1){
-			//fileName = 'configurations.json';
-			//return;
-	//	}
-	//}
 	
-	fs.readFile(fileName, 'binary', function(err, file){
-		//console.log('serving file: ' + fileName);
-		if (err) {
-			console.log('error for file : ' + fileName + ' err: ' + err);
-			write500InternalErrorResponse(res, err, contentType);
-			return;
-		}
-		//console.log('SERVING FILE: ' + fileName);
-		write200SuccessResponse(res, file, contentType);
+	switch (uri){
+		case '/':
+			if (fs.statSync(fileName).isDirectory()) {
+				fileName += 'src/index.html';
+			}
+			
+			renderFile(res, fileName, contentType);
+			break;
+		case '/user-configurations':
+			//res.writeHead(200);
+			//res.write('200 OK');
+			if (token){
+				
+			}
+			res.end();
+			break;
+		case '/configs':
+			res.setHeader('Content-Type', 'application/json');
+			res.write(JSON.stringify(configs));
+			res.end();
+			break;
+		case '/attemptLogin':
+			var credentials = url.parse(req.url, true).query;
 		
-		return;
-	});
+			if (credentials){
+				var username = credentials.username;
+				var password = credentials.password;
+				
+				if (!validate(username, password)){
+					write401Unauthorized(res);
+					return;
+				} else {
+					token = new Buffer('username:' + username + ',' + 'password:' + password).toString('base64');
+					write200SuccessResponse(res, null, contentType);
+					return;
+				}
+			}
+		
+			break;
+		default:
+			res.writeHead(404);
+			res.write('404 Not Found');
+			res.end();
+	}
 	
-//	console.log('failed on :::'  + fileName + ' content-type: ' + contentType);
-	return;
-	 
+		 
 }).listen(parseInt(PORT, 10)); 
 
 console.log('Server running at --> http://localhost:' + PORT + '/\nCTRL+C to shutdown');
