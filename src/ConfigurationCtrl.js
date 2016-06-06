827d1010d99050ca90f4ed32f354d8564007234f
Chad Keibler <chad.keibler@aesynt.com>
2016-06-06 14:03:53 -0400

    moved js css and html files to src folder

---
+++ src/ConfigurationCtrl.js
@@ -0,0 +1,16 @@
+angular.module('sampleApp.controllers', []).
+controller('ConfigurationCtrl', function($scope) {
+	$scope.totalStairs = null;
+	
+	$scope.logout = function (){
+		//window.location.href = '/';
+	}
+	
+	$scope.isValid = function(){
+		
+	}
+	
+	$scope.showStairs = function(total) {
+		
+	}
+});
