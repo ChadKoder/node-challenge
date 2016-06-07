4ee58d6e82c2b1c80623856b004e0d568faa6e3b
Chad Keibler <chad.keibler@aesynt.com>
2016-06-07 10:35:30 -0400

    cleaned up display of configurations some. updated ctrl to put data

--- src/ConfigurationCtrl.js
+++ src/ConfigurationCtrl.js
@@ -1,6 +1,7 @@
 angular.module('sampleApp.controllers', []).
 controller('ConfigurationCtrl', function($scope, $http, $mdToast) {
 	var redirectDelay = 1000;
+	$scope.title = 'Node.js Sample Application';
 	
 	$scope.logout = function (){
 		$http({
@@ -13,10 +14,6 @@ controller('ConfigurationCtrl', function($scope, $http, $mdToast) {
 			$scope.showSimpleToast('lougout failed... strange...');
 		});
 	};
-	
-	$scope.selectedConfig = null;
-	$scope.editing = false;
-	$scope.title = 'Node.js Sample Application';
 	
 	$scope.showSimpleToast = function (msg){
 		$mdToast.show(
@@ -37,42 +34,70 @@ controller('ConfigurationCtrl', function($scope, $http, $mdToast) {
 	};
 	
 	$scope.updateConfig = function(){
-		
+		var req = {
+			method: 'PUT',
+			url: '/configs',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			data: { test: $scope.selectedConfig }
+		};
+
+		var req = {
+			method: 'PUT',
+			url: '/configs',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			data: { config: $scope.selectedConfig }
+		};
+		$http(req).success(function(){
+			alert('success');
+		}).error(function(){
+			alert('failure');
+		});
+		/*($http.put('/configs', $scope.selectedConfig)
+			.then(function (res) {
+				$scope.showSimpleToast('Updated configurations!');
+			}, function () {
+				$scope.showSimpleToast('Failed to update configuration');
+		});*/
 	};
 	
 	$scope.deleteConfig = function(){
 		var url =  '/configs/?' + $scope.selectedConfig.username;
-		$http({
-			method: 'DELETE',
-			url: url
-		}).then(function (res) {
-			$scope.configs = $scope.getConfigs();
-			$scope.selectedConfig = null;
-			$scope.showSimpleToast('Configuration deleted');
-		}, function (res) {
-			$scope.showSimpleToast('Deletion failed');
-		});
+		$http.delete(url)
+			.then(function (res) {
+				$scope.configs = $scope.getConfigs();
+				$scope.selectedConfig = null;
+				$scope.showSimpleToast('Configuration deleted');
+			}, function () {
+				$scope.showSimpleToast('Deletion failed');
+			});
+	};
+	
+	$scope.showAdd = function(){
+		$scope.showAddConfig = true;
 	};
 	
 	$scope.getConfigs = function() {
-		var url =  '/configs';
-		$http({
-			method: 'GET',
-			url: url
-		}).then(function (res) {
-			$scope.configs = res.data.configurations;
-		
-		}, function (res) {
-			if (res.status === 401){
-				$scope.showSimpleToast('Unauthorized user!');
-			} else {
-				$scope.showSimpleToast('unknown error has occurred.');
-			}
-		});
+		$http.get('/configs')
+			.then(function (res) {
+				$scope.configs = res.data.configurations;
+			}, function (res){
+				if (res.status === 401){
+					$scope.showSimpleToast('Unauthorized user!');
+				} else {
+					$scope.showSimpleToast('Unable to retrieve configs.');
+				}
+			});
 	};
 	
 	$scope.init = function(){
 		$scope.configs = $scope.getConfigs();
+		$scope.showAddConfig = false;
+		$scope.selectedConfig = null;
+		$scope.editing = false;
 	};
 	
 	$scope.init();
