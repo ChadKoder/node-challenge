2f726a7b4eb3cbda5ef6e6b19aeeeb80deed2334
Chad Keibler <chad.keibler@aesynt.com>
2016-06-07 13:15:49 -0400

    adding unit tests for http calls

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -3,17 +3,38 @@ var server = require('../server');
 var api = supertest('http://localhost:8888');
 
 describe('server', function() {
-	describe('test1', function() {
-		it('should do something', function() {
-			expect(1).toEqual(1);
+	describe('Unauthorized request', function(){
+		describe('GET /validateUser with no credentials', function(){
+			it('should return status code 401 Unauthorized', function(done){
+				 api.get('/validateUser')
+					.set('Accept', 'application/json')
+					.expect(401, done);
+			});
+		});
+		
+		describe('GET /user-configurations with no credentials', function(){
+			it('should return status code 401 Unauthorized', function(done){
+				 api.get('/user-configurations')
+					.set('Accept', 'application/json')
+					.expect(401, done);
+			});
+		});
+		
+		describe('GET /configs with no credentials', function(){
+			it('should return status code 401 Unauthorized', function(done){
+				 api.get('/configs')
+					.set('Accept', 'application/json')
+					.expect(401, done);
+			});
+		});
+		
+		describe('GET / with no credentials', function(){
+			it('should return status code 200 OK', function(done){
+				 api.get('/')
+					.set('Accept', 'application/json')
+					.expect(200, done);
+			});
 		});
 	});
 	
-	describe('GET /attemptLogin with no credentials', function(){
-		it('should return status code 401 Unauthorized', function(done){
-			 api.get('/attemptLogin')
-				.set('Accept', 'application/json')
-				.expect(401, done);
-		});
-	});
 });
