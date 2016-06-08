f4dcf350506f7b3d958f5e460459c55fb10f9a33
Chad Keibler <chad.keibler@aesynt.com>
2016-06-08 16:15:53 -0400

    broke out unit test runners into separate files because I couldnt get them running together with the way the project is setup. This will do for now.  Also added some more tests for configurationctrl

--- unitTests/ConfigurationCtrl.spec.js
+++ unitTests/ConfigurationCtrl.spec.js
@@ -2,43 +2,158 @@ describe('ConfigurationCtrl', function () {
     var ctrl,
         $rootScope,
         $scope,
-		$httpBackend,
+		//var newConfig = '{"config":{"username":"d","name":"g","hostname":"r","port":"c"}}';
+		responseNoSort = { sorted: [{a:'a'}] ,total: 7 },
+		responseSort = { sorted: [{a:'b'}] ,total: 23 },
 		$mdToast,
-		$window,
-		response = 'blah',;
+		$httpBackend;
 
     beforeEach(function(){
 		module('sampleApp');
 	});
 
     beforeEach(function () {
-		//$mdToast = jasmine.createSpyObj('$mdToast', ['showSimple']);
-		//callbackSpy = jasmine.createSpy('callbackSpy');
-		
+		$mdToast = jasmine.createSpyObj('$mdToast', ['showSimple']);
         inject(function ($injector, $controller, _$httpBackend_) {
             $rootScope = $injector.get('$rootScope');
 			$scope = $rootScope.$new();
-			//$window = $injector.get('$window');
-            $httpBackend = _$httpBackend_;
+			$httpBackend = _$httpBackend_;
 			
-            ctrl = $controller('ConfigurationCtrl', {
-                $scope: $scope
-            });			 
+			ctrl = $controller('ConfigurationCtrl', {
+				$scope: $scope,
+				$mdToast: $mdToast
+			});
         });
-    });
-
-    /*describe('$scope.logout()', function () {
-        beforeEach(function () {
-			$httpBackend.expectPOST('/logout').respond(response);
-			$scope.logout();
-        });
-
-        it('should display toast message', function () {
+		
+		$httpBackend.expectGET('/configs?page=1&sortby=name').respond(responseNoSort);
+	});
+	
+	/*afterEach(function() {
+		$httpBackend.flush();
+		$httpBackend.verifyNoOutstandingExpectation();
+		$httpBackend.verifyNoOutstandingRequest();
+	})*/;
+		 
+	describe('$scope.init()', function(){
+		beforeEach(function(){
+			spyOn($scope, 'getConfigs');
+			$scope.init();
+		});
+		
+		it('should call $scope.getConfigs', function(){
+			expect($scope.getConfigs).toHaveBeenCalled();
+		});
+		
+		it ('should default page, sortValue, totalDisplayed, and pageSize', function(){
+			expect($scope.adding).toBeFalsy();
+			expect($scope.selectedConfig).toBe(null);
+			expect($scope.editing).toBeFalsy();
+			expect($scope.page).toEqual(1);
+			expect($scope.pageSize).toEqual(5);
+			expect($scope.totalDisplayed).toEqual(0);
+		});
+	});
+	 	
+	describe('$scope.editConfg', function(){
+		it('should set $scope.selectedConfig', function (){
+			$scope.selectedConfig = 'NotEdit';
+			$scope.editConfig('edit');
+			expect($scope.selectedConfig).toBe('edit');
+		});
+	});
+	
+	describe('$scope.sort', function(){
+		beforeEach(function(){
+			spyOn($scope, 'getConfigs');
+			$scope.sort('name');
+		});
+		
+		it('should call $scope.getConfigs', function(){
+			expect($scope.getConfigs).toHaveBeenCalledWith('name');
+		});
+	});
+	
+	describe('$scope.getConfigs with no sort value', function(){
+		beforeEach(function(){
+			$scope.sortValue = null;
+			$scope.pageSize = 5;
+			$scope.page = 0;
+			$scope.totalDisplayed = 0;
+			$scope.totalConfigs = 0;
+			$scope.configs = null;
+			$httpBackend.expectGET('/configs?page=1&sortby=name').respond(responseNoSort);
+			$scope.getConfigs(null);
+		});
+		
+		it('should set the sort value and pageSize', function(){
 			$httpBackend.flush();
 			$httpBackend.verifyNoOutstandingExpectation();
 			$httpBackend.verifyNoOutstandingRequest();
-        });
-    });*/
+			expect($scope.sortValue).toBe(null); 
+			expect($scope.totalDisplayed).toBe($scope.pageSize); 
+			expect($scope.page).toBe(1); 
+			expect($scope.totalConfigs).toBe(7); 
+			expect($scope.configs.length).toEqual(1);
+		});
+	});
+	
+	describe('$scope.getConfigs with a sort value', function(){
+		beforeEach(function(){
+			$scope.sortValue = null;
+			$scope.pageSize = 5;
+			$scope.page = 0;
+			$scope.totalDisplayed = 0;
+			$scope.totalConfigs = 0;
+			$scope.configs = null;
+			$httpBackend.expectGET('/configs?page=1&sortby=name').respond(responseSort);
+			$scope.getConfigs('name');
+		});
+		
+		it('should set the sort value and pageSize', function(){
+			$httpBackend.flush();
+			$httpBackend.verifyNoOutstandingExpectation();
+			$httpBackend.verifyNoOutstandingRequest();
+			expect($scope.sortValue).toBe('name'); 
+			expect($scope.totalDisplayed).toBe($scope.pageSize); 
+			expect($scope.page).toBe(1); 
+			expect($scope.totalConfigs).toBe(23); 
+			expect($scope.configs.length).toEqual(1);
+		});
+	});
+	
+	describe('$scope.getConfigs with http 401 Unauthorized', function () {
+		beforeEach(function(){
+			$scope.sortValue = null;
+			$scope.pageSize = 5;
+			$scope.page = 0;
+			$scope.totalDisplayed = 0;
+			$scope.totalConfigs = 0;
+			$scope.configs = null;
+			$httpBackend.expectGET('/configs?page=1&sortby=name').respond(401);
+			$scope.getConfigs('name');
+			
+		});
+		
+		it ('should show error msg', function(){
+			$httpBackend.flush();
+			$httpBackend.verifyNoOutstandingExpectation();
+			$httpBackend.verifyNoOutstandingRequest();
+			
+			expect($mdToast.showSimple).toHaveBeenCalledWith('Unauthorized user!');
+		});
+		
+	});
 	
 	
+	//describe('$scope.logout', function(){
+		//it ('should make hhtp call GET /logout', function (){
+			//var ctrl = createController();
+			//$httpBackend.expectPOST('/logout').respond(response);
+			//$scope.logout();
+		//	//$httpBackend.flush();
+			//$httpBackend.verifyNoOutstandingRequest();
+			//$httpBackend.verifyNoOutstandingExpectation();
+		//});
+		
+	//});
 });
