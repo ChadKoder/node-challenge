f3967e333db6e357c9f592cd1fea2304617204fc
Chad Keibler <chad.keibler@aesynt.com>
2016-06-12 12:52:19 -0400

    cherry-picking commit from 'cleanup-and-more-tests' branch to display server tests nicely, and restructured the server tests to read a little better

--- serverTests/server-auth.spec.js
+++ serverTests/server-auth.spec.js
@@ -1,38 +1,63 @@
 var supertest = require('supertest');
 var api = supertest('http://localhost:8888');
+var login = function (done) {
+		 api.get('/validateUser')
+				.set('Authorization', 'Basic Y2hhZGs6VGVuYWJsZQ==')
+				.expect(200, done);
+	};
+	
+beforeEach(function(done){
+	login(done);
+});
 
-describe('server', function() {
-	describe('When a request is authorized', function(){
-		it('should return status code 200 OK', function(done){
+describe('server authorized', function() {
+	describe('http GET', function(){
+		it('/validateUser should return status code 200 OK', function(done){
 			 api.get('/validateUser')
 				.set('Authorization', 'Basic Y2hhZGs6VGVuYWJsZQ==')
 				.expect(200, done);
 		});
 		
-		it('GET /user-configruations should return status code 200 OK', function(done){
+		it('/user-configruations should return status code 200 OK', function(done){
 			 api.get('/user-configurations')
 				.set('Accept', 'text/html')
 				.expect(200, done);
 		});
 		
-		it('GET /configs should return status code 200 OK', function(done){
+		it('/configs should return status code 200 OK', function(done){
 			 api.get('/configs')
 				.set('Accept', 'application/json')
 				.expect(200, done);
 		});
 	
-		it('GET / should return status code 200 OK', function(done){
+		it('/ should return status code 200 OK', function(done){
 			 api.get('/')
 				.set('Accept', 'application/json')
 				.expect(200, done);
 		});
 		
-		it('DELETE /configs should return status code 204 No Content', function(done){
+		it('then GET /invalidUrl should return status code 404 Not Found', function(done){
+			 api.get('/invalidUrl')
+				.set('Accept', 'application/json')
+				.expect(404, done);
+		});
+	});
+	
+	describe('http DELETE', function(){
+		it('/configs should return status code 204 No Content', function(done){
 			 api.delete('/configs')
 				.set('Accept', 'application/json')
 				.expect(204, done);
 		});
+		
+		it('then DELETE /invalidUrl should return status code 404 Not Found', function(done){
+			 api.delete('/invalidUrl')
+				.set('Accept', 'application/json')
+				.expect(404, done);
+		});	
+	});
 	
+	describe('http POST', function(){
 		it('should return status code 204 No Content', function(done){
 			var newConfig = '{"config":{"username":"d","name":"g","hostname":"r","port":"c"}}';
 			api.post('/configs')
@@ -42,37 +67,23 @@ describe('server', function() {
 				.expect(204, done);
 		});
 		
-		describe('and the url is invalid', function(){
-			it('should return status code 200 OK', function(done){
-				 api.get('/validateUser')
-					.set('Authorization', 'Basic Y2hhZGs6VGVuYWJsZQ==')
-					.expect(200, done);
-			});
-			
-			it('then PUT /invalidUrl should return status code 404 Not Found', function(done){
-				 api.put('/invalidUrl')
-					.set('Accept', 'application/json')
-					.expect(404, done);
-			});
-			
-			it('then GET /invalidUrl should return status code 404 Not Found', function(done){
-				 api.get('/invalidUrl')
-					.set('Accept', 'application/json')
-					.expect(404, done);
-			});
-			
-		
-			it('then POST /invalidUrl should return status code 404 Not Found', function(done){
-				 api.post('/invalidUrl')
-					.set('Accept', 'application/json')
-					.expect(404, done);
-			});
-			
-			it('then DELETE /invalidUrl should return status code 404 Not Found', function(done){
-			 api.delete('/invalidUrl')
+		it('then POST /invalidUrl should return status code 404 Not Found', function(done){
+			 api.post('/invalidUrl')
 				.set('Accept', 'application/json')
 				.expect(404, done);
-			});	
 		});
+		
 	});
-}); 
+	
+	describe('http PUT', function(){
+			
+		it('then PUT /invalidUrl should return status code 404 Not Found', function(done){
+			 api.put('/invalidUrl')
+				.set('Accept', 'application/json')
+				.expect(404, done);
+		});
+		
+	});
+	
+ 
+});
--- serverTests/server-unauth.spec.js
+++ serverTests/server-unauth.spec.js
@@ -1,68 +1,69 @@
 var supertest = require('supertest');
-var api = supertest('http://localhost:8888');
-var http = require('http');
-
-describe('server', function() {
-	describe('Unauthorized request', function(){
-		beforeEach(function(){
-			var options = {
-				  hostname: 'http://localhost:8888',
-				  port: 8888,
-				  path: '/logout',
-				  method: 'POST',
-				  headers: {
-					'Content-Type': 'application/json'
-				  }
-			};
-			
-			//logout for tests to ensure unauthorized..
-			var req = http.request(options, function (res) {
-			});
-			
-			req.write('');
-			req.end();
-		});
-		it('GET /validateUser should return status code 401 Unauthorized', function(done){
+	var api = supertest('http://localhost:8888');
+	
+var logout = function (done) {
+		 api.post('/logout').expect(204, done);
+};
+	
+describe('server unauthorized', function() {
+	beforeEach(function(done){ 
+		//logout to ensure unauthorized..
+		logout(done);
+	});
+	
+	describe('http GET', function(){
+		it ('/validateUser should return status code 401 Unauthorized', function(done){
 			 api.get('/validateUser')
-				.set('Accept', 'application/json')
-				.expect(401, done);
+			.set('Accept', 'application/json')
+			.expect(401, done);
 		});
-		
-		it('GET /user-configruations should return status code 401 Unauthorized', function(done){
+
+		it('/user-configruations should return status code 401 Unauthorized', function(done){
 			 api.get('/user-configurations')
 				.set('Accept', 'application/json')
 				.expect(401, done);
 		});
-		 
-		
-		it('GET /configs should return status code 401 Unauthorized', function(done){
+			
+		it('/configs should return status code 401 Unauthorized', function(done){
 			 api.get('/configs')
 				.set('Accept', 'application/json')
 				.expect(401, done);
 		});
 				
-		it('GET / should return status code 200 OK', function(done){
+		it('/ should return status code 200 OK', function(done){
 			 api.get('/')
 				.set('Accept', 'application/json')
 				.expect(200, done);
 		});
+		
+		it('/invalidUrl should return status code 404 Not Found', function(done){
+			 api.get('/invalidUrl')
+				.set('Accept', 'application/json')
+				.expect(404, done);
+		});
+	});
 	
-		it('DELETE /configs should return status code 200 OK', function(done){
-			 api.delete('/configs')
-				.set('Accept', 'application/json')
-				.expect(401, done);
+	describe('http DELETE', function(){
+		it('/configs should return status code 401 Unauthorized', function(done){
+		 api.delete('/configs')
+			.set('Accept', 'application/json')
+			.expect(401, done);
 		});
-		
-		it('should return status code 401 Unauthorized', function(done){
-			 api.post('/configs')
-				.set('Accept', 'application/json')
-				.expect(401, done);
+	});
+	
+	describe('http POST', function(){
+		it('/configs should return status code 401 Unauthorized', function(done){
+		 api.post('/configs')
+			.set('Accept', 'application/json')
+			.expect(401, done);
 		});
-		
-		it('should return status code 200 OK', function(done){
+	
+		it('/logout should return status code 204 No Content', function(done){
 			 api.post('/logout')
 				.set('Accept', 'application/json')
 				.expect(204, done);
 		});
+		
+		
 	});
 });
