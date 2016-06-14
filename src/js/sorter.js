77b4cdf8e9af502e7452f4155013667194ad9035
Chad Keibler <chad.keibler@aesynt.com>
2016-06-14 16:23:56 -0400

    made sorter testable, grouped asc and desc functions together

--- src/js/sorter.js
+++ src/js/sorter.js
@@ -1,47 +1,11 @@
-module.exports = function (userConfigs) {
+function Sorter (userConfigs) {
 	return {
-		sortByNameDesc: function (){
-			return userConfigs.configurations.sort(function(x, y) {
-			if (x.name.toLowerCase() > y.name.toLowerCase()){
-				return -1;
-			}
-			if (x.name.toLowerCase() < y.name.toLowerCase()){
-				return 1;
-			}
-			
-			return 0;
-			}); 
-		},
 		sortByHostNameAsc: function(){
 			return userConfigs.configurations.sort(function(x, y) {
 			if (x.hostname.toLowerCase() < y.hostname.toLowerCase()){
 				return -1;
 			}
 			if (x.hostname.toLowerCase() > y.hostname.toLowerCase()){
-				return 1;
-			}
-			
-			return 0;
-			});
-		},
-		sortByHostNameDesc: function(){
-			return userConfigs.configurations.sort(function(x, y) {
-			if (x.hostname.toLowerCase() > y.hostname.toLowerCase()){
-				return -1;
-			}
-			if (x.hostname.toLowerCase() < y.hostname.toLowerCase()){
-				return 1;
-			}
-			
-			return 0;
-			});
-		},
-		sortByPortDesc: function(){
-			return userConfigs.configurations.sort(function(x, y) {
-			if (x.port > y.port){
-				return -1;
-			}
-			if (x.port < y.port){
 				return 1;
 			}
 			
@@ -60,18 +24,6 @@ module.exports = function (userConfigs) {
 				return 0;
 			});
 		},
-		sortByUserNameDesc: function (){
-			return userConfigs.configurations.sort(function(x, y) {
-				if (x.username.toLowerCase() > y.username.toLowerCase()){
-					return -1;
-				}
-				if (x.username.toLowerCase() < y.username.toLowerCase()){
-					return 1;
-				}
-				
-				return 0;
-			});
-		},
 		sortByPortAsc: function(){
 			return userConfigs.configurations.sort(function(x, y) {
 			if (x.port < y.port){
@@ -83,21 +35,6 @@ module.exports = function (userConfigs) {
 			
 			return 0;
 			});
-		},
-		getSortDesc: function (sortBy){
-			var sorted = null;
-			if (sortBy.toLowerCase() === 'hostname') {
-				sorted = this.sortByHostNameDesc();
-			} else if (sortBy.toLowerCase() === 'port') {
-				sorted = this.sortByPortDesc();
-			} else if (sortBy.toLowerCase() === 'username') {
-				sorted = this.sortByUserNameDesc();
-			} else {
-				//default - sort by name desc
-				sorted = this.sortByNameDesc();
-			}
-			
-			return sorted;
 		},
 		sortByNameAsc: function (){
 			return userConfigs.configurations.sort(function(x, y) {
@@ -125,6 +62,73 @@ module.exports = function (userConfigs) {
 			}
 			
 			return sorted;
+		},
+		sortByNameDesc: function (){
+			return userConfigs.configurations.sort(function(x, y) {
+			if (x.name.toLowerCase() > y.name.toLowerCase()){
+				return -1;
+			}
+			if (x.name.toLowerCase() < y.name.toLowerCase()){
+				return 1;
+			}
+			
+			return 0;
+			}); 
+		},
+		sortByHostNameDesc: function(){
+			return userConfigs.configurations.sort(function(x, y) {
+			if (x.hostname.toLowerCase() > y.hostname.toLowerCase()){
+				return -1;
+			}
+			if (x.hostname.toLowerCase() < y.hostname.toLowerCase()){
+				return 1;
+			}
+			
+			return 0;
+			});
+		},
+		sortByPortDesc: function(){
+			return userConfigs.configurations.sort(function(x, y) {
+			if (x.port > y.port){
+				return -1;
+			}
+			if (x.port < y.port){
+				return 1;
+			}
+			
+			return 0;
+			});
+		},
+		sortByUserNameDesc: function (){
+			return userConfigs.configurations.sort(function(x, y) {
+				if (x.username.toLowerCase() > y.username.toLowerCase()){
+					return -1;
+				}
+				if (x.username.toLowerCase() < y.username.toLowerCase()){
+					return 1;
+				}
+				
+				return 0;
+			});
+		},
+		getSortDesc: function (sortBy){
+			var sorted = null;
+			if (sortBy.toLowerCase() === 'hostname') {
+				sorted = this.sortByHostNameDesc();
+			} else if (sortBy.toLowerCase() === 'port') {
+				sorted = this.sortByPortDesc();
+			} else if (sortBy.toLowerCase() === 'username') {
+				sorted = this.sortByUserNameDesc();
+			} else {
+				//default - sort by name desc
+				sorted = this.sortByNameDesc();
+			}
+			
+			return sorted;
 		}
 	}
-};
+}
+
+module.exports = Sorter;
+ 
+	
