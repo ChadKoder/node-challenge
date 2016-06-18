0aa887552f727332552ec4a25753c8e06a220834
Chad Keibler <chad.keibler@aesynt.com>
2016-06-18 09:08:25 -0400

    added authRouter spec

--- unitTests/authRouter.spec.js
+++ unitTests/authRouter.spec.js
@@ -1,11 +1,31 @@
 describe('AuthRouter', function (){
 	var authRouter, path, responseService, 
-	currentWorkingDir = 'c:\',
+	currentWorkingDir = 'c:\\',
 	configPageObjCreator, auth,
-	res = {}, req = {},  url  = '',
+	res = { write: function () {}, writeHead: function () {}, end: function() {} }, req = { headers: []},  url  = '',
 	path = { join: function(a, b) { return a + '/' + b; } };
 	var c = function (err, file) { if (err) { return 'error'; } return 'noError' ; };
 	var fs = { readFile: function(a, b, readFileError) { return; } };
+	var userConfigs = {
+		"configurations": [{
+				"name": "zack",
+				"hostname": "aaa",
+				"port": 7777,
+				"username": "bname"
+			},
+			{
+				"name": "anthony",
+				"hostname": "zzz",
+				"port": 1234,
+				"username": "zname"
+			},
+			{
+				"name": "john",
+				"hostname": "ggg",
+				"port": 1235,
+				"username": "uname"
+			}]
+	};
 	
 	beforeEach(function() {
 		responseService = new ResponseService();
@@ -17,8 +37,23 @@ describe('AuthRouter', function (){
 	describe('routeGet', function() {
 		describe('when uri is /validateUser and no authorization header is provided', function () {
 			it('should return 401 Unauthorized', function() {
+				spyOn(responseService, 'write401Unauthorized');
+				spyOn(auth, 'validateUser');
 				authRouter.routeGet('fName', '/validateUser', res, req, 'contentType');
+				expect(auth.validateUser).not.toHaveBeenCalled();
+				expect(responseService.write401Unauthorized).toHaveBeenCalled();
 			});
 		});
+		
+		/*describe('when uri is /validateUser with authorization header', function () {
+			it('should call authentication.validateUser', function() {
+				spyOn(auth, 'validateUser');
+				req.headers['authorization'] = 'Basic hash';
+				
+				
+				authRouter.routeGet('fName', '/validateUser', res, req, 'contentType');
+				expect(auth.validateUser).toHaveBeenCalled();
+			});
+		});*/
 	});
 });
