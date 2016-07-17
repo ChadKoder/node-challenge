e2128be3bd7435b3d4aa119b14a7d7efcd614c53
Chad Keibler <chad.keibler@aesynt.com>
2016-07-17 07:22:41 -0400

    fixed loading of canvas, need to fix display now.

--- src/js/controllers/MainCtrl.js
+++ src/js/controllers/MainCtrl.js
@@ -16,7 +16,7 @@ angular.module('MainCtrl', []).controller('MainCtrl', ['$window','$scope', '$htt
 				url: 'http://192.168.1.109:8888/photo',
 				data: img
 			};
-								 
+
 			 $http(req).success(function(){
 				 alert('photo saved successfully!');
 				 
@@ -73,29 +73,79 @@ angular.module('MainCtrl', []).controller('MainCtrl', ['$window','$scope', '$htt
 		};
               
 		vm.getBase64Image = function (imgElem) {
-			var canvas = document.createElement("canvas");
+			var canvas = document.createElement('canvas');
 			canvas.width = imgElem.clientWidth;
 			canvas.height = imgElem.clientHeight;
-			var ctx = canvas.getContext("2d");
+			var ctx = canvas.getContext('2d');
 			ctx.drawImage(imgElem, 0, 0);
 			var dataURL = canvas.toDataURL("image/png");
 			return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 		};
-		
+                                                       
+        vm.createCanvasElement = function(img){
+            var parent = document.getElementById('images');
+            var canvas = document.createElement('canvas');
+            //canvas.setAttribute('id', 'canvas' + ctr);
+             
+            canvas.width = 20;
+            canvas.height =20;
+            parent.appendChild(canvas);
+            
+            var context = canvas.getContext('2d');
+            context.drawImage(img, 0, 0);
+            var dataUrl = canvas.toDataURL('image/png');
+            $scope.images.push(dataUrl);
+                                                       
+                                                      
+            if (!$scope.$$phase) {
+                $scope.$apply();
+            }
+        };
+        
 		vm.showCameraRoll = function () {
 			 $window.imagePicker.getPictures(function(results) {
 				 for (var i = 0; i < results.length; i++){
+					//vm.createCanvasElement(results[i]);
                     $scope.images.push(results[i]);
-					vm.loadCanvas(results[i], i);
+                  // vm.createCanvasElement(results[i], i);
+					//vm.loadCanvas(results[i], i);
                   }
 			 });
+             
 		};
 		 
+        vm.loadImages = function() {
+			var parent = document.getElementById('images');
+            var canvas = document.createElement('canvas');
+			canvas.setAttribute('id', 'canvas');
+             var context = canvas.getContext('2d');
+			
+			//	canvas.setAttribute('style', 'width:250px; height: 250px;');
+            // canvas.style.border = '1px solid';
+           var img = new Image();
+		   img.src = $scope.images[0];
+           img.onload = function() {
+			   canvas.width = img.width;
+			   canvas.height = img.height;
+               context.drawImage(this, 0, 0, img.width, img.height);
+			}
+            
+            parent.appendChild(canvas);
+                                                       
+           if (!$scope.$$phase) {
+               $scope.$apply();
+           }
+                                                       
+            //for (var j = 0; j < $scope.images.length; j++) {
+              //  vm.createCanvasElement($scope.images[0]);
+            //   }
+        };
+
 		vm.submitPhotos = function () {
-            var img = new Image();
-            var canvas = document.getElementById('canvas');
-            var encoded = canvas.toDataURL('image/png|gif|jpg|jpeg');
-            vm.sendPhoto(encoded);
+           // var img = new Image();
+           var canvas = document.getElementById('canvas');
+           var encoded = canvas.toDataURL('image/png|gif|jpg|jpeg');
+           vm.sendPhoto(encoded);
 			// for (var i = 0; i < $scope.images.length; i++){
 				//vm.sendPhoto($scope.images[i]);
 			// }
