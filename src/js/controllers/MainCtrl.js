1a90a7885e03272b56b7226cbdce51b018cc45c3
Chad Keibler <chad.keibler@aesynt.com>
2016-07-20 10:44:12 -0400

    fixed problem with loading multiple images. added check upon completion of send

--- src/js/controllers/MainCtrl.js
+++ src/js/controllers/MainCtrl.js
@@ -7,6 +7,7 @@ angular.module('MainCtrl', []).controller('MainCtrl', ['$window','$scope', '$htt
 	vm.totalSent = 0;
 	vm.inProgress = false;
 	$scope.album = [];
+    vm.album = null;
 
 	vm.clear = function() {
 		for (var i = 0; i < $scope.album.length; i++){
@@ -24,6 +25,7 @@ angular.module('MainCtrl', []).controller('MainCtrl', ['$window','$scope', '$htt
 
 		$http(req).success(function(){
 			vm.totalSent++;
+			
 			if (vm.totalSent === $scope.album.length) {
 				vm.inProgress = false;
 				vm.showSimpleToast('Photo saved successfully!');
@@ -34,34 +36,47 @@ angular.module('MainCtrl', []).controller('MainCtrl', ['$window','$scope', '$htt
 			vm.showSimpleToast('Failed to save photo.');
 		});
 	};
+    
+    
+    vm.loadImage = function(img, index) {
+		img.onload = function() {
+			var canvas = document.getElementById('canvas' + index);
+            var context = canvas.getContext('2d');
 
-	vm.showCameraRoll = function () {
-		$window.imagePicker.getPictures(function(results) {
-			for (var i = 0; i < results.length; i++){
-				var image = new Image();
-				image.src = results[i];
-
-				var photo = {
-					index: i
-				};
-
-				$scope.album.push(photo);
-				vm.refresh();
-
-				var el = document.getElementById('canvas' + i);
-				var context = el.getContext('2d');
-
-				image.onload = function(){
-					el.width = image.width;
-					el.height = image.height;
-					context.drawImage(this, 0, 0);
-				}
-
-				vm.refresh();
-			}
-		});
-
-	};
+            canvas.width = img.width;
+            canvas.height = img.height;
+            context.drawImage(this, 0, 0);
+        }
+    };
+    
+    vm.callback = function() {
+            for (var i = 0; i < $scope.album.length; i++){
+                var img = new Image();
+                img.src = $scope.album[i].image;
+                             
+                vm.loadImage(img, i);
+            }
+    };
+    
+    vm.showCameraRoll = function() {
+        $window.imagePicker.getPictures(function (results){
+            for (var i = 0; i < results.length; i++){
+                var photo = {
+                    index: i,
+                    image: results[i]
+                }
+                
+                 $scope.album.push(photo);
+            }
+            
+            vm.refresh();
+            vm.callback();
+        });
+    };
+    
+    vm.selectFromRoll = function() {
+        vm.showCameraRoll();
+    };
 
 	vm.refresh = function () {
 		if (!$scope.$$phase) {
